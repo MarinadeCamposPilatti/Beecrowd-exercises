ca81ee13c7ecbab3bc81de033e55b0f19f23b268
Marina de campos pilatti <104401610+MarinadeCamposPilatti@users.noreply.github.com>
2022-11-04 21:36:29 -0300

    Update Exercise1002(Área do Círculo).java

--- Exercise1002(Área do Círculo).java
+++ Exercise1002(Área do Círculo).java
@@ -10,6 +10,17 @@ Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exempl
 Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, 
 caso contrário, você receberá "Presentation Error".
 
+
+The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+
+Calculate the area using the formula given in the problem description.
+
+Input
+The input contains a value of floating point (double precision), that is the variable R.
+
+Output
+Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point.
+Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error".
 */
 import java.io.IOException;
 import java.util.Scanner;
