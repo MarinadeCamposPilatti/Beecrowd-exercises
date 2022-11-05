546b9d7e6fcb33c49699f19ef36a8ceb34347237
Marina de campos pilatti <104401610+MarinadeCamposPilatti@users.noreply.github.com>
2022-11-04 21:37:19 -0300

    Update Exercise1003(Soma Simples).java

--- Exercise1003(Soma Simples).java
+++ Exercise1003(Soma Simples).java
@@ -7,6 +7,16 @@ O arquivo de entrada contém 2 valores inteiros.
 Saída
 Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B.
 Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+Read two integer values, in this case, the variables A and B. After this, calculate the sum between them and assign it to the variable SOMA. 
+Write the value of this variable.
+
+Input
+The input file contains 2 integer numbers.
+
+Output
+Print the message "SOMA" with all the capital letters, with a blank space before and after the equal signal followed by the corresponding value to the sum of A and B. 
+Like all the problems, don't forget to print the end of line, otherwise you will receive "Presentation Error"
 */
 
 import java.io.IOException;
