a3a599c9a663c13b115161ad7262a4d33e210fd5
Marina de campos pilatti <104401610+MarinadeCamposPilatti@users.noreply.github.com>
2022-11-04 21:31:17 -0300

    Update Exercise1001(Extremamente Básico).java

--- Exercise1001(Extremamente Básico).java
+++ Exercise1001(Extremamente Básico).java
@@ -8,6 +8,17 @@ A entrada contém 2 valores inteiros.
 Saída
 Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
 Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+
+
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+Obs.: don't forget the endline after all.
 */
 
 import java.util.Scanner;
